0914b6f27ee070b0a2ddc1eb934ff15cb9c13a49
Kevin Daza <kevin.daza@baumsoft.co>
2026-04-24 00:05:49 -0500

    Fix: Improve deploy router configuration

--- routes/deployRouter.js
+++ routes/deployRouter.js
@@ -1,48 +1,71 @@
 import { exec } from 'node:child_process'
+import crypto from 'node:crypto'
 
-export const deployRouter = async (req, res) => {
+export const deployRouter = (req, res) => {
   const myUrl = new URL(req.url, `http://${req.headers.host}`)
   const { pathname } = myUrl
 
   if (pathname === '/deploy' && req.method === 'POST') {
-    const secret = req.headers['x-deploy-secret']
-
-    if (secret !== 'mi-secreto-deploy') {
-      res.writeHead(403)
-      return res.end('Forbidden')
-    }
-
-    let body = ''
+    let rawBody = ''
 
     req.on('data', chunk => {
-      body += chunk.toString()
+      rawBody += chunk
     })
 
     req.on('end', () => {
       try {
-        const data = JSON.parse(body)
+        const signature = req.headers['x-hub-signature-256']
 
-        if (data.ref !== 'refs/heads/main') {
-          res.writeHead(200)
-          return res.end('Ignored: not main branch')
+        if (!signature) {
+          res.writeHead(401)
+          return res.end('No signature')
         }
 
-        exec('cd /root/repos/crud-node-js && git pull origin main && pm2 restart server', (err, stdout, stderr) => {
-          if (err) {
-            console.error(err)
-            res.writeHead(500)
-            return res.end('Deploy failed')
-          }
+        const hmac = crypto
+          .createHmac('sha256', 'secret-webhook-node') // SECRET
+          .update(rawBody)
+          .digest('hex')
 
-          console.log(stdout)
+        const expected = `sha256=${hmac}`
+
+        const isValid = crypto.timingSafeEqual(
+          Buffer.from(signature),
+          Buffer.from(expected)
+        )
+
+        if (!isValid) {
+          res.writeHead(403)
+          return res.end('Invalid signature')
+        }
+
+        const data = JSON.parse(rawBody)
+
+        if (data.ref !== 'refs/heads/master') {
           res.writeHead(200)
-          res.end('Deploy successful')
-        })
+          return res.end('Ignored: not master branch')
+        }
+
+        exec(
+          'cd /root/repos/crud-node-js && git pull origin master && pm2 restart server',
+          (err, stdout, stderr) => {
+            if (err) {
+              console.error(err)
+              res.writeHead(500)
+              return res.end('Deploy failed')
+            }
+
+            console.log(stdout)
+            res.writeHead(200)
+            return res.end('Deploy successful')
+          }
+        )
       } catch (error) {
+        console.error(error)
         res.writeHead(400)
-        res.end('Invalid payload')
+        return res.end('Invalid payload')
       }
     })
+
     return
   }
 
